1483ab98ae7331b3ee63e06c03efdcbcf88ba518
Sneha <ranisneha599@gmail.com>
2024-05-27 15:39:35 +0530

    add cookies config

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -5,14 +5,7 @@ import axios from "axios";
 
 const BASE_URL = `https://edunity-pvt-ltd-api.onrender.com`;
 
-const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true, // This ensures cookies are sent with requests
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    timeout: 10000, // 10 seconds timeout
-});
+const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.withCredentials = true;
